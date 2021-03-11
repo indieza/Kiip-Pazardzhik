728469faf7b1484171e94c3b5335d62dfe6b9dd2
indieza <simeonvalev00@gmail.com>
2021-03-11 12:29:23 +0200

    Implement people table

--- KiipPazardzhik/KiipPazardzhik/wwwroot/js/peopleScript.js
+++ KiipPazardzhik/KiipPazardzhik/wwwroot/js/peopleScript.js
@@ -1,1 +1,34 @@
-﻿
+﻿function changeVisibility(filterValue, buttonElement) {
+    if ([...buttonElement.classList].includes("freeFilterSpan")) {
+        buttonElement.classList.remove("freeFilterSpan");
+
+        let oldButtons = document.querySelectorAll(".filterSpan");
+
+        for (var button of oldButtons) {
+            button.classList.remove("filterSpan");
+            button.classList.add("freeFilterSpan");
+        }
+
+        buttonElement.classList.add("filterSpan");
+    }
+
+    if (filterValue == "Всички") {
+        let rows = document.querySelectorAll("tbody>tr");
+
+        for (let row of rows) {
+            row.style.display = "";
+        }
+    }
+    else {
+        let rows = document.querySelectorAll(`.${filterValue}`);
+        let allRows = document.querySelectorAll("tbody>tr");
+
+        for (let row of allRows) {
+            row.style.display = "none";
+        }
+
+        for (let row of rows) {
+            row.parentElement.style.display = "";
+        }
+    }
+}
